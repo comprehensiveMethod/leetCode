82632dd4eb925b70339fb15815a07ae882805965
Timur <timur32240@gmail.com>
2024-11-22 22:03:09 +0700

    1 medium algorithm task solved

---
+++ src/LongestSubstribgNoReapiting/Solution.java
@@ -0,0 +1,24 @@
+package LongestSubstribgNoReapiting;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0;
+        int maxLength = 0;
+        HashSet<Character> charSet = new HashSet<>();
+
+        for (int right = 0; right < s.length(); right++) {
+            while (charSet.contains(s.charAt(right))) {
+                charSet.remove(s.charAt(left));
+                left++;
+            }
+
+            charSet.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
